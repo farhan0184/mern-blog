1214dac7b95bc6eb67dea032a4cb8f895186142f
farhan0184 <shahariar0184@gmail.com>
2024-05-11 03:49:40 +0600

    create sign-in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 export const signup = async  (req, res, next )=>{ // next is middleware
     const {username, email, password} = req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === ''){
         // return res.status(400).json({message: "All fields are required"});
-        next(errorHandler( 400 ,"All fields are required"));
+        return next(errorHandler( 400 ,"All fields are required"));
 
     }
 
@@ -27,5 +28,31 @@ export const signup = async  (req, res, next )=>{ // next is middleware
     }
     
 
+}
+
+export const signin = async(req, res, next)=>{
+
+    const {email, password} = req.body;
+    if(!email || !password || email === "" || password === ""){
+        return next(errorHandler( 400 ,"All fields are required"));
+    }
+
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser){
+           return next(errorHandler( 404 ,"User not found"));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword){
+            next(errorHandler( 400 ,"Invalid password"));
+        }
+        const {password:pass, ...rest} = validUser._doc
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET, {expiresIn: '1d'})
+        res.status(200).cookie('access_token', token, {httpOnly: true}).json({data:rest, token: token})
+    } catch (error) {
+        next(error)
+    }
 }
 
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -24,7 +24,7 @@ const currentYear = new Date().getFullYear();
 export function Footer() {
     return (
         <footer className="relative w-full  border-t-8 border-teal-500 rounded-t-lg">
-            <div className="mx-auto w-full max-w-7xl px-8">
+            <div className="mx-auto w-full  px-8">
                 <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2 mt-8">
                     <div>
                         <Logo />
@@ -60,7 +60,7 @@ export function Footer() {
                         variant="small"
                         className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
                     >
-                        &copy; {currentYear} <a href="https://material-tailwind.com/">Material Tailwind</a>. All
+                        &copy; {currentYear} <a href="#">Material Tailwind</a>. All
                         Rights Reserved.
                     </Typography>
                     <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
