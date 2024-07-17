edbd1ed4f9d69c74d0295908b3b63ee8c9fbee24
farhan0184 <shahariar0184@gmail.com>
2024-07-18 05:57:16 +0600

    complete create post function

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -3,24 +3,30 @@ import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import CustomBtn from '../components/CustomBtn';
-import { getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreatePost() {
+    const [publishError, setPublishError] = useState(null)
     const [file, setfile] = useState(null)
     const [imageUploadProgress, setImageUploadProgress] = useState(null)
     const [imageUploadError, setImageUploadError] = useState(null)
     const [formData, setFormData] = useState({})
-    const handleUploadImage = () =>{
+    const navigate = useNavigate()
+    // console.log(formData)
+    // image upload
+    const handleUploadImage = () => {
         try {
-            if(!file){
+            if (!file) {
                 setImageUploadError("Please select an image");
                 return;
             }
             const storage = getStorage(app)
-            const fileName = new Date().getTime() + "-"+ file.name;
+            const fileName = new Date().getTime() + "-" + file.name;
             // console.log(fileName)
             const storageRef = ref(storage, fileName);
             const uploadTask = uploadBytesResumable(storageRef, file);
@@ -50,45 +56,73 @@ export default function CreatePost() {
         }
 
     }
-    console.log(imageUploadProgress)
+    
+    // handle submit 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        // console.log(formData)
+        try {
+            const response = await axios.post('/api/post/create', formData)
+            // console.log(response)
+            if (response?.statusText === 'Created') {
+                setPublishError(null)
+                navigate(`/post/${response?.data?.data?.slug}`)
+              }else{
+                setPublishError(response?.data?.message)
+              }
+        } catch (error) {
+            setPublishError(error?.response?.data.message)
+        }
+    }
     return (
-        <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+        <div className=' max-w-3xl mx-auto my-20'>
             <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-            <form className='space-y-7'>
-                <div className='flex flex-col gap-5 sm:flex-row justify-between'>
-                    <Input size="lg" color="teal" type="text" label="Title" className='flex-1' labelProps={{
-                        className: "",
-                    }} />
-                    <Select size="lg" label="Select Version" color="teal">
-                        <Option>Material Tailwind HTML</Option>
-                        <Option>Material Tailwind React</Option>
-                        <Option>Material Tailwind Vue</Option>
-                        <Option>Material Tailwind Angular</Option>
-                        <Option>Material Tailwind Svelte</Option>
-                    </Select>
+            <form className='space-y-7' onSubmit={handleSubmit}>
+                <div className='w-full flex flex-col gap-5 sm:flex-row justify-between'>
+                    <div className='w-1/2'>
+                        <Input size="lg" color="teal" type="text" label="Title" className='' labelProps={{
+                            className: "",
+                        }} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
+                    </div>
+                    {/* <Select size="lg" label="Select Version" color="teal" 
+                    onChange={(e) => console.log(e.target.value)}
+                    >
+                        <Option>Option 1</Option>
+                        <Option>Option 3</Option>
+                        <Option>Option 4</Option>
+                        <Option>Option 5</Option>
+                    </Select> */}
+                    <select name="" id="" className='border-[1px] border-gray-400 p-2 outline-none rounded-lg w-1/2 ' onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
+                        <option value="uncategorized">Select a Category</option>
+                        <option value="reactjs">React Js</option>
+                        <option value="vuejs">Vue Js</option>
+                        <option value="Nextjs">Next Js</option>
+                    </select>
                 </div>
                 <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted py-2'>
-                    <input type="file" className='border-[1px] p-2 focus:border-teal-500 rounded-lg ' onChange={(e) => setfile(e.target.files[0])}/>
+                    <input type="file" className='border-[1px] p-2 focus:border-teal-500 rounded-lg ' onChange={(e) => setfile(e.target.files[0])} />
                     <Button color="teal" variant="outlined" className='w-max' onClick={handleUploadImage}
                     >
                         {imageUploadProgress ? <div className='w-10 h-10'>
-                            <CircularProgressbar 
-                                value={imageUploadProgress} 
+                            <CircularProgressbar
+                                value={imageUploadProgress}
                                 text={`${imageUploadProgress || 0}%`}
                             />
                         </div> : "Upload Image"}
                     </Button>
                 </div>
                 {imageUploadError && <Alert color="red">{imageUploadError}</Alert>}
-                {formData.image &&(
+                {formData.image && (
                     <img src={formData.image} alt="" className='w-full h-72 object-cover' />
                 )}
-                <ReactQuill theme="snow" className='h-[200px] pb-12' required />
+                <ReactQuill theme="snow" className='h-[200px] pb-12' required
+                    onChange={(value) => setFormData({ ...formData, content: value })}
+                />
                 <div>
                     <CustomBtn title="Publish Post" loading={false} />
                 </div>
             </form >
-
+            {publishError && <Alert color="red" className='mt-5'>{publishError}</Alert>}
 
         </div >
     )
