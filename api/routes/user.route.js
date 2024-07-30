be8eea9b5177c90ba61cedf715b3dbf35dd14ddb
farhan0184 <shahariar0184@gmail.com>
2024-07-30 12:13:24 +0600

    add all user route in api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {deleteUser, Signout, test, updateUser} from '../controllers/user.controller.js';
+import {deleteUser, getUsers, Signout, test, updateUser} from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ router.get('/test', test)
 router.put('/update/:userId', verifyUser, updateUser)
 router.delete('/delete/:userId', verifyUser, deleteUser)
 router.post('/signout',Signout)
+router.get('/user-list', verifyUser, getUsers )
 
 export default router
