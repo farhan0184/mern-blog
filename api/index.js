14f8ec125aa4c3b54491f74f263119dd4e7f1dfc
farhan0184 <shahariar0184@gmail.com>
2024-08-05 02:25:32 +0600

    pre installation add

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 
 // after install env - npm i env
@@ -20,6 +21,7 @@ await mongoose
         console.log(err)
     })
 
+const __dirname = path.resolve()
 
 const app = express();
 
@@ -51,7 +53,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
 
+app.use(express.static(path.join(__dirname, '/client/dist')))
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 // next middleware
 app.use((err, req, res, next) => {
     const statuscode = err.statuscode || 500
